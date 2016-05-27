f65538ec4b10015767b3c4b7a166090e882123b9
Alfa Ryano Yohannis <ary506@york.ac.uk>
2016-05-27 16:25:46 +0100

    Update support for EOL

--- gamified-modelling-server-0/src/org/york/gamified/modelling/Main.java
+++ gamified-modelling-server-0/src/org/york/gamified/modelling/Main.java
@@ -2,11 +2,14 @@ package org.york.gamified.modelling;
 
 import java.io.IOException;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sun.org.apache.xml.internal.resolver.helpers.Debug;
 
 /**
  * Servlet implementation class Main
@@ -14,34 +17,52 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(description = "Main Class for Gamified Modelling", urlPatterns = { "/Main" })
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Main() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see Servlet#init(ServletConfig)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	public void init(ServletConfig config) throws ServletException {
+	public Main() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+//	/**
+//	 * @see Servlet#init(ServletConfig)
+//	 */
+//	public void init(ServletConfig config) throws ServletException {
+//		super();
+//	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at:
+		// ").append(request.getContextPath());
+
+		// Get the absolute path of the image
+		try {
+			String filename = getServletContext().getRealPath("/");
+			response.getWriter().append(filename);
+			
+			Eol eol = new Eol();
+			eol.execute();
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
